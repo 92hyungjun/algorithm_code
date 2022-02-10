22b04fe7a33d8b8d6d3f85657dc70cdfdfe4571b
92hyungjun <78071764+92hyungjun@users.noreply.github.com>
2022-02-10 21:57:56 +0900

    Update 15649.py

--- baejoon/backtracking/15649.py
+++ baejoon/backtracking/15649.py
@@ -1,1 +1,38 @@
-1
+#https://www.acmicpc.net/problem/15649
+import copy
+#print("enter N, M")
+N, M = map(int, input().split())
+#N = 4
+#M = 2
+
+def contain( list, val ):
+    for i in list:
+        if( val == i ):
+            return True
+
+    return False
+
+def print_cnt( val, parents ):
+    #print(val, "", end="")
+    if( len(parents) == M ):
+        for i in parents:
+            print(i,"",end="")
+        print()
+        return
+
+    for i in range(N):
+        print_val = i + 1
+        if contain(parents, print_val):
+            continue
+        new_parents = copy.deepcopy(parents)
+        new_parents.append(print_val)
+        if( len(new_parents) > M ):
+            break
+        print_cnt( print_val, new_parents )
+        #print()
+
+
+
+for i in range(N):
+    print_cnt(i+1, [i+1] )
+    #print()
